d3ab1f8254ce6ba4235f08824a437c74c8ba1b97
Taylor Britton <k.taylor.britton@gmail.com>
2016-08-24 12:20:36 -0700

    finishes todo list using controller as method

--- js/todosController.js
+++ js/todosController.js
@@ -3,8 +3,17 @@ angular.module('todosApp')
 
 function TodosController () {
   var self = this;
+  self.addTodo = addTodo;
 
+  self.newTodo = {
+    text: '',
+    done: false
+  }
 
+  function addTodo() {
+    self.todos.push({text: self.newTodo.text, done: false});
+    self.newTodo = {text: '', done: false};
+  }
 
   self.todos = [
     {text: 'Get some Broccoli', done: false},
